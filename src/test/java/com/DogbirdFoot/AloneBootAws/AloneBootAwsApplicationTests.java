31aa736519ba92e3603454407cb404489f265c41
seol <audmslpl@gmail.com>
2022-06-06 21:11:49 +0900

    V.2 git ignore 셋팅

---
+++ src/test/java/com/DogbirdFoot/AloneBootAws/AloneBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DogbirdFoot.AloneBootAws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloneBootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7a902fed75839346a78b6e8a2f09b57a1f74101d
seol <audmslpl@gmail.com>
2022-06-07 23:29:17 +0900

    V.2 git ignore 셋팅

---
+++ src/main/java/com/DogbirdFoot/AloneBootAws/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.DogbirdFoot.AloneBootAws.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
---
+++ src/test/java/com/DogbirdFoot/AloneBootAws/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.DogbirdFoot.AloneBootAws.web.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트() {
+        String name ="test";
+        int amount =1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        Assertions.assertThat(dto.getName()).isEqualTo(name);
+        Assertions.assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}

2184d21b8acdaed5813ef5b0a20f083645df3c3a
seol <audmslpl@gmail.com>
2022-06-18 16:22:16 +0900

    V3.Spring Security Oauth 부분 적용

--- src/main/java/com/DogbirdFoot/AloneBootAws/web/IndexController.java
+++ src/main/java/com/DogbirdFoot/AloneBootAws/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.DogbirdFoot.AloneBootAws.web;
 
+import com.DogbirdFoot.AloneBootAws.config.auth.LoginUser;
+import com.DogbirdFoot.AloneBootAws.config.auth.dto.SessionUser;
 import com.DogbirdFoot.AloneBootAws.service.posts.PostService;
 import com.DogbirdFoot.AloneBootAws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostService postService;
+    private final HttpSession httpSession;
 
     @GetMapping("/posts/save")
     public String postsSave(){
@@ -19,8 +24,11 @@ public class IndexController {
     }
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model ,@LoginUser SessionUser user){
         model.addAttribute("posts",postService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
